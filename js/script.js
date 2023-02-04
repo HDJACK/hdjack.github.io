9315a5c7a62941dbe2ea724e9bb39a9fdeb7d57d
HDJACK <hdjackyt0@gmail.com>
2023-02-05 00:01:49 +0100

    content remove

--- js/script.js
+++ js/script.js
@@ -51,14 +51,7 @@ dtBtn.addEventListener("click", function () {
 
 
 $(document).ready(() => {
-    $("#gangsBtn").click(function () {
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active");
-        } else {
-            $(this).addClass("active");
-            loadHtml();
-        }
-    });
+    loadHtml();
 
     $("img").click(function (e) {
         let x = e.pageX - e.target.offsetLeft;
@@ -82,126 +75,154 @@ async function loadHtml() {
     let counter = 0;
     let html = "";
     const data = await getData();
+    if ($('#gangsBtn').prop('checked')) {
+        data.gangs.forEach((gang) => {
+            let blip = "";
+            let description = "";
+            blip += `<div class="item-point circle" data-top="${gang.position.y}" data-left="${gang.position.x}" style="background: rgb(${gang.blip_color.r},${gang.blip_color.g},${gang.blip_color.b})" data-popover="#gang-${counter}">`;
+            blip += `<div>`;
+            blip += `<a href="#" class="toggle" style="width: 20px; height: 20px;"></a>`;
+            blip += `</div>`;
+            blip += `</div>`;
 
-    data.gangs.forEach((gang) => {
-        let blip = "";
-        let description = "";
-        blip += `<div class="item-point circle" data-top="${gang.position.y}" data-left="${gang.position.x}" style="background: rgb(${gang.blip_color.r},${gang.blip_color.g},${gang.blip_color.b})" data-popover="#gang-${counter}">`;
-        blip += `<div>`;
-        blip += `<a href="#" class="toggle" style="width: 20px; height: 20px;"></a>`;
-        blip += `</div>`;
-        blip += `</div>`;
+            description += `<div id="gang-${counter}" class="content right bottom">`;
+            description += `<div class="head">`;
+            description += `<a href="#" class="exit"><img src="assets/close.png" alt="" style="color:"#000" /></a>`;
+            description += `<h6 class="title">${gang.name}</h6>`;
+            description += `</div>`;
+            description += `<div class="body">`;
+            description += `<p><img src="${gang.description.skin}" alt=" "/></p>`;
+            description += `<p>`;
+            gang.description.texts.forEach(text => {
+                description += `<b>${text.title}:</b> ${text.content}<br>`;
+            });
+            description += `</p>`;
+            description += `</div>`;
+            description += `</div>`;
 
-        description += `<div id="gang-${counter}" class="content right bottom">`;
-        description += `<div class="head">`;
-        description += `<a href="#" class="exit"><img src="assets/close.png" alt="" style="color:"#000" /></a>`;
-        description += `<h6 class="title">${gang.name}</h6>`;
-        description += `</div>`;
-        description += `<div class="body">`;
-        description += `<p><img src="${gang.description.skin}" alt=" "/></p>`;
-        description += `<p>`;
-        gang.description.texts.forEach(text => {
-            description += `<b>${text.title}:</b> ${text.content}<br>`;
+            html += blip + description;
+
+            counter++;
         });
-        description += `</p>`;
-        description += `</div>`;
-        description += `</div>`;
+        $('#content').html($('#content').html() + html);
+        $(".scalize").scalize();
+    } else {
+        $('#content').empty();
+    }
+    if ($('#squadBtn').prop('checked')) {
+        data.squads.forEach((squad) => {
+            let blip = "";
+            let description = "";
+            blip += `<div class="item-point square" data-top="${squad.position.y}" data-left="${squad.position.x}" style="background: rgb(${squad.blip_color.r},${squad.blip_color.g},${squad.blip_color.b})" data-popover="#squad-${counter}">`;
+            blip += `<div>`;
+            blip += `<a href="#" class="toggle" style="width: 10px; height: 10px;"></a>`;
+            blip += `</div>`;
+            blip += `</div>`;
 
-        html += blip + description;
+            description += `<div id="squad-${counter}" class="content right bottom">`;
+            description += `<div class="head">`;
+            description += `<a href="#" class="exit"><img src="assets/close.png" alt="" style="color:"#000" /></a>`;
+            description += `<h6 class="title">${squad.name}</h6>`;
+            description += `</div>`;
+            description += `<div class="body">`;
+            description += `<p>`;
+            squad.description.texts.forEach(text => {
+                description += `<b>${text.title}:</b> ${text.content}<br>`;
+            });
+            description += `</p>`;
+            description += `</div>`;
+            description += `</div>`;
 
-        counter++;
-    });
-    data.squads.forEach((squad) => {
-        let blip = "";
-        let description = "";
-        blip += `<div class="item-point square" data-top="${squad.position.y}" data-left="${squad.position.x}" style="background: rgb(${squad.blip_color.r},${squad.blip_color.g},${squad.blip_color.b})" data-popover="#squad-${counter}">`;
-        blip += `<div>`;
-        blip += `<a href="#" class="toggle" style="width: 10px; height: 10px;"></a>`;
-        blip += `</div>`;
-        blip += `</div>`;
+            html += blip + description;
 
-        description += `<div id="squad-${counter}" class="content right bottom">`;
-        description += `<div class="head">`;
-        description += `<a href="#" class="exit"><img src="assets/close.png" alt="" style="color:"#000" /></a>`;
-        description += `<h6 class="title">${squad.name}</h6>`;
-        description += `</div>`;
-        description += `<div class="body">`;
-        description += `<p>`;
-        squad.description.texts.forEach(text => {
-            description += `<b>${text.title}:</b> ${text.content}<br>`;
+            counter++;
         });
-        description += `</p>`;
-        description += `</div>`;
-        description += `</div>`;
+        $('#content').html($('#content').html() + html);
+        $(".scalize").scalize();
+    } else {
+        $('#content').empty();
+    }
+    if ($('#wtBtn').prop('checked')) {
+        data.wt.forEach((item) => {
+            const { position, name } = item;
+            let blip = "";
+            let description = "";
+            blip += `<div class="item-point circle" data-top="${position.y}" data-left="${position.x}" style="background-image: url('assets/Blip_501.webp'); background-size: cover; background-color: rgba(0,0,0,0); border: 0;" data-popover="#wt-${counter}">`;
+            blip += `<div>`;
+            blip += `<a href="#" class="toggle" style="width: 20px; height: 20px;"></a>`;
+            blip += `</div>`;
+            blip += `</div>`;
 
-        html += blip + description;
+            description += `<div id="wt-${counter}" class="content right bottom">`;
+            description += `<div class="head">`;
+            description += `<a href="#" class="exit"><img src="assets/close.png" alt="" style="color:"#000" /></a>`;
+            description += `<h6 class="title">WT: ${name}</h6>`;
+            description += `</div>`;
+            description += `</div>`;
 
-        counter++;
-    });
-    data.wt.forEach((item) => {
-        const { position, name } = item;
-        let blip = "";
-        let description = "";
-        blip += `<div class="item-point circle" data-top="${position.y}" data-left="${position.x}" style="background-image: url('assets/Blip_501.webp'); background-size: cover; background-color: rgba(0,0,0,0); border: 0;" data-popover="#wt-${counter}">`;
-        blip += `<div>`;
-        blip += `<a href="#" class="toggle" style="width: 20px; height: 20px;"></a>`;
-        blip += `</div>`;
-        blip += `</div>`;
+            html += blip + description;
 
-        description += `<div id="wt-${counter}" class="content right bottom">`;
-        description += `<div class="head">`;
-        description += `<a href="#" class="exit"><img src="assets/close.png" alt="" style="color:"#000" /></a>`;
-        description += `<h6 class="title">WT: ${name}</h6>`;
-        description += `</div>`;
-        description += `</div>`;
 
-        html += blip + description;
+            counter++;
+        });
+        $('#content').html($('#content').html() + html);
+        $(".scalize").scalize();
+    } else {
+        $('#content').empty();
+    }
+    if ($('#atBtn').prop('checked')) {
+        data.at.forEach((item) => {
+            const { position, name } = item;
+            let blip = "";
+            let description = "";
+            blip += `<div class="item-point circle" data-top="${position.y}" data-left="${position.x}" style="background-image: url('assets/Blip_67.webp'); background-size: cover; background-color: rgba(0,0,0,0); border: 0;" data-popover="#at-${counter}">`;
+            blip += `<div>`;
+            blip += `<a href="#" class="toggle" style="width: 20px; height: 20px;"></a>`;
+            blip += `</div>`;
+            blip += `</div>`;
 
-        counter++;
-    });
-    data.at.forEach((item) => {
-        const { position, name } = item;
-        let blip = "";
-        let description = "";
-        blip += `<div class="item-point circle" data-top="${position.y}" data-left="${position.x}" style="background-image: url('assets/Blip_67.webp'); background-size: cover; background-color: rgba(0,0,0,0); border: 0;" data-popover="#at-${counter}">`;
-        blip += `<div>`;
-        blip += `<a href="#" class="toggle" style="width: 20px; height: 20px;"></a>`;
-        blip += `</div>`;
-        blip += `</div>`;
+            description += `<div id="at-${counter}" class="content right bottom">`;
+            description += `<div class="head">`;
+            description += `<a href="#" class="exit"><img src="assets/close.png" alt="" style="color:"#000" /></a>`;
+            description += `<h6 class="title">AT: ${name}</h6>`;
+            description += `</div>`;
+            description += `</div>`;
 
-        description += `<div id="at-${counter}" class="content right bottom">`;
-        description += `<div class="head">`;
-        description += `<a href="#" class="exit"><img src="assets/close.png" alt="" style="color:"#000" /></a>`;
-        description += `<h6 class="title">AT: ${name}</h6>`;
-        description += `</div>`;
-        description += `</div>`;
+            html += blip + description;
 
-        html += blip + description;
+            counter++;
+        });
+        $('#content').html($('#content').html() + html);
+        $(".scalize").scalize();
+    } else {
+        $('#content').empty();
+    }
+    if ($('#dtBtn').prop('checked')) {
+        data.dt.forEach((item) => {
+            const { position, name } = item;
+            let blip = "";
+            let description = "";
+            blip += `<div class="item-point circle" data-top="${position.y}" data-left="${position.x}" style="background-image: url('assets/Blip_140.webp'); background-size: cover; background-color: rgba(0,0,0,0); border: 0;" data-popover="#dt-${counter}">`;
+            blip += `<div>`;
+            blip += `<a href="#" class="toggle" style="width: 20px; height: 20px;"></a>`;
+            blip += `</div>`;
+            blip += `</div>`;
 
-        counter++;
-    });
-    data.dt.forEach((item) => {
-        const { position, name } = item;
-        let blip = "";
-        let description = "";
-        blip += `<div class="item-point circle" data-top="${position.y}" data-left="${position.x}" style="background-image: url('assets/Blip_140.webp'); background-size: cover; background-color: rgba(0,0,0,0); border: 0;" data-popover="#dt-${counter}">`;
-        blip += `<div>`;
-        blip += `<a href="#" class="toggle" style="width: 20px; height: 20px;"></a>`;
-        blip += `</div>`;
-        blip += `</div>`;
+            description += `<div id="dt-${counter}" class="content right bottom">`;
+            description += `<div class="head">`;
+            description += `<a href="#" class="exit"><img src="assets/close.png" alt="" style="color:"#000" /></a>`;
+            description += `<h6 class="title">DT: ${name}</h6>`;
+            description += `</div>`;
+            description += `</div>`;
 
-        description += `<div id="dt-${counter}" class="content right bottom">`;
-        description += `<div class="head">`;
-        description += `<a href="#" class="exit"><img src="assets/close.png" alt="" style="color:"#000" /></a>`;
-        description += `<h6 class="title">DT: ${name}</h6>`;
-        description += `</div>`;
-        description += `</div>`;
+            html += blip + description;
 
-        html += blip + description;
+            counter++;
+        });
 
-        counter++;
-    });
-
-    $('#content').html($('#content').html() + html);
-    $(".scalize").scalize();
+        $('#content').html($('#content').html() + html);
+        $(".scalize").scalize();
+    } else {
+        $('#content').empty();
+    }
 }
